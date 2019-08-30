472c4f9bddf80b3a12463ffe83773404bfae1eeb
Iqbal Adriansyah <Iqbal.adriansyah@bankmandiri.co.id>
2019-08-30 11:22:14 +0700

    initial commit

---
+++ TestJfrog/TestJfrog.h
@@ -0,0 +1,19 @@
+//
+//  TestJfrog.h
+//  TestJfrog
+//
+//  Created by IQ on 30/08/19.
+//  Copyright © 2019 Iqbal Adriansyah Adrial. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestJfrog.
+FOUNDATION_EXPORT double TestJfrogVersionNumber;
+
+//! Project version string for TestJfrog.
+FOUNDATION_EXPORT const unsigned char TestJfrogVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestJfrog/PublicHeader.h>
+
+
